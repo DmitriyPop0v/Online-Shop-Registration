456677009434f83bfdb5f5b2e3ac53be05a8fc33
DmitriyPop0v <popov.for.study@gmail.com>
2022-11-18 18:20:10 +0300

    Frontend - css, js, some php for verification. Js for ajax queries

---
+++ js/actions.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+
+	/*
+		Here #login is login form id and this form is available in index.php page
+		from here input data is sent to login.php page
+		if you get login_success string from login.php page means user is logged in successfully and window.location is 
+		used to redirect user from home page to profile.php page
+	*/
+	$("#login").on("submit",function(event){
+		event.preventDefault();
+		$(".overlay").show();
+		$.ajax({
+			url	:	"login.php",
+			method:	"POST",
+			data	:$("#login").serialize(),
+			success	:function(data){
+				if(data == "login_success"){
+					window.location.href = "index.php";
+				}else if(data == "cart_login"){
+					window.location.href = "cart.php";
+				}else{
+					$("#e_msg").html(data);
+					$(".overlay").hide();
+				}
+			}
+		})
+	})
+	
+	//Get User Information before checkout
+	$("#signup_form").on("submit",function(event){
+		event.preventDefault();
+		$(".overlay").show();
+		$.ajax({
+			url : "register.php",
+			method : "POST",
+			data : $("#signup_form").serialize(),
+			success : function(data){
+				$(".overlay").hide();
+				if (data == "register_success") {
+					window.location.href = "cart.php";
+				}else{
+					$("#signup_msg").html(data);
+				}
+				
+			}
+		})
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
